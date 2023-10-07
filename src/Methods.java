d1830080f279e8d8c7eac073a2935aab634901e0
C20334466 <C20334466@mytudublin.ie>
2023-10-07 03:43:52 +0100

    Initial Commit

---
+++ src/Methods.java
@@ -0,0 +1,10 @@
+// abtract interface reated for methids for retrieving friends information
+public abstract interface Methods {
+	
+	// Method to retrieve name of friend
+	public String getName();
+	
+	// Method to retrieve user id of friend
+	public long getUserId();
+
+}
